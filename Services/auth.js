5a7f3a1f929f47ebc50e28f99a438e8fb9a8895e
Praveen Kumar <pkumar21@paypal.com>
2022-03-24 21:27:16 +0530

    heroku port fix

--- Services/auth.js
+++ Services/auth.js
@@ -72,29 +72,33 @@ let login = async (req, res) => {
 };
 
 let health = async (req, res) => {
-  http.get('http://127.0.0.1:8080/v1/health', (resp) => {
-    let data = '';
-
-    console.log("here");
-    // A chunk of data has been received.
-    resp.on('data', (chunk) => {
-      data += chunk;
-      console.log("here2 =>" + data);
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      //console.log(JSON.parse(data).explanation);
-      console.log("here3");
-      var name = "Praveen";
+  var name = "Praveen";
       res.render('pages/test', {
-          name: data
+          name: name
       });
-    });
+  // http.get('http://127.0.0.1:8080/v1/health', (resp) => {
+  //   let data = '';
 
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
+  //   console.log("here");
+  //   // A chunk of data has been received.
+  //   resp.on('data', (chunk) => {
+  //     data += chunk;
+  //     console.log("here2 =>" + data);
+  //   });
+
+  //   // The whole response has been received. Print out the result.
+  //   resp.on('end', () => {
+  //     //console.log(JSON.parse(data).explanation);
+  //     console.log("here3");
+  //     var name = "Praveen";
+  //     res.render('pages/test', {
+  //         name: data
+  //     });
+  //   });
+
+  // }).on("error", (err) => {
+  //   console.log("Error: " + err.message);
+  // });
   
 };
 
